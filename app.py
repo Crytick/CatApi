d4dc11aedced9eda461f822e3b68fd84e9f3e4ed
Crytick <caleb.eixmann@gmail.com>
2022-10-13 17:04:40 -0500

    added picture to flask website

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def cats():
-    return {"message": "cats!"}
+    URL = 'https://api.thecatapi.com/v1/images/search'
+    r = requests.get(URL)
+    json = r.json()
+    url = json[0]["url"]
+
+    return "<img src = '" + url + "'></img>"
+
+
+
+
+
+
+
+
+# --------------------------------------------
+
+# import requests
+
+# URL = 'https://api.thecatapi.com/v1/images/search'
+# r = requests.get(URL)
+# json = r.json()
+# url = json[0]["url"]
+# print(url)
